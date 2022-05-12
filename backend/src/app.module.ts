5adb51d1c9a723204de75f92e57dd672d06b34da
Alex M <alexmunoz91@gmail.com>
2022-05-12 22:54:39 +0200

    Add new features: - Add ConfigModule - Add MongoseModule - Add NftsModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { NftsModule } from './nfts/nfts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+
+    NftsModule,
+  ],
 })
 export class AppModule {}
