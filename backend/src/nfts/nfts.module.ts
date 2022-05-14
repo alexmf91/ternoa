70c6f1b08e81f187096c4d0d52f48bf0fe6ac18c
Alex M <alexmunoz91@gmail.com>
2022-05-14 16:30:20 +0200

    Add nft MongooseModule

--- backend/src/nfts/nfts.module.ts
+++ backend/src/nfts/nfts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { NftsService } from './nfts.service';
 import { NftsController } from './nfts.controller';
+import { Nft, NftSchema } from './schemas/nft.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Nft.name, schema: NftSchema }])],
   controllers: [NftsController],
   providers: [NftsService],
 })
