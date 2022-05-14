ee935ea8eacd6417059b588eaa1e334b82bf5b92
Alex M <alexmunoz91@gmail.com>
2022-05-14 16:28:11 +0200

    Add nft schema

---
+++ backend/src/nfts/schemas/nft.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type NftDocument = Nft & Document;
+
+@Schema()
+export class Nft {
+  @Prop({ type: String, required: true })
+  title: string;
+
+  @Prop({ type: String, required: true })
+  description: string;
+
+  @Prop({ type: String, required: true })
+  picture: string;
+}
+
+export const NftSchema = SchemaFactory.createForClass(Nft);
