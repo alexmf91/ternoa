eb3850d3c6286d54f351f265625d4508e4e44aaa
Alex M <alexmunoz91@gmail.com>
2022-05-14 16:29:29 +0200

    Add nft service CRUD functions

--- backend/src/nfts/nfts.service.ts
+++ backend/src/nfts/nfts.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Nft, NftDocument } from './schemas/nft.schema';
+
 import { CreateNftDto } from './dto/create-nft.dto';
 import { UpdateNftDto } from './dto/update-nft.dto';
 
 @Injectable()
 export class NftsService {
-  create(createNftDto: CreateNftDto) {
-    return 'This action adds a new nft';
+  constructor(@InjectModel(Nft.name) private nftModel: Model<NftDocument>) {}
+
+  public createOneNft(createNftDto: CreateNftDto) {
+    const createdNft = new this.nftModel(createNftDto);
+    return createdNft.save();
   }
 
-  findAll() {
-    return `This action returns all nfts`;
+  public findAllNfts(): Promise<Array<Nft>> {
+    return this.nftModel.find().exec();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} nft`;
+  public async findOneNft(nftId: string) {
+    const existingNft = await this.nftModel.findById(nftId).exec();
+
+    if (!existingNft) {
+      throw new NotFoundException(`Nft #${nftId} not found`);
+    }
+
+    return existingNft;
   }
 
-  update(id: number, updateNftDto: UpdateNftDto) {
-    return `This action updates a #${id} nft`;
+  public async updateNft(nftId: any, updateNftDto: UpdateNftDto) {
+    const existingNft = await this.nftModel.findByIdAndUpdate(
+      nftId,
+      updateNftDto,
+      { new: true },
+    );
+
+    if (!existingNft) {
+      throw new NotFoundException(`Nft #${nftId} not found`);
+    }
+
+    return existingNft;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} nft`;
+  public async deleteNft(nftId: string) {
+    const deletedNft = await this.nftModel.findByIdAndDelete(nftId);
+
+    if (!deletedNft) {
+      throw new NotFoundException(`Nft #${nftId} not found`);
+    }
+
+    return deletedNft;
   }
 }
