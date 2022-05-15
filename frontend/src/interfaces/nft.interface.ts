d97f62290415425ab7642bee69b5941ff5a5d0a6
Alex M <alexmunoz91@gmail.com>
2022-05-15 19:54:43 +0200

    Add nft interfaces

---
+++ frontend/src/interfaces/nft.interface.ts
@@ -0,0 +1,12 @@
+export interface Nft {
+  _id: string;
+  name: string;
+  picture: string;
+  description: string;
+}
+
+export interface NftToApi {
+  name: string;
+  picture: string;
+  description: string;
+}
