dc14ed830beea7efc024ba7df521cec197abe3cd
Alex M <alexmunoz91@gmail.com>
2022-05-15 20:11:47 +0200

    Add components index module

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,8 @@
+import Header from './Header/Header.component';
+import NftCard from './NftCard/NftCard.component';
+import NftDetails from './NftDetails/NftDetails.component';
+import NftList from './NftList/NftList.component';
+import AddAndEditNft from './AddAndEditNft/AddAndEditNft.component';
+import Spinner from './Spinner/Spinner.component';
+
+export { Header, NftCard, NftDetails, NftList, AddAndEditNft, Spinner };
